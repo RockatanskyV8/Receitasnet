e1102eafd6cc0a9fa5dec70621f1e7bd513da03f
Lucas Santos Rosa <luccasanttoz@gmail.com>
2018-11-30 20:59:59 -0200

    Criando filtros costumizados

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.created_at
   end
 
   def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   def light?
     calories < 100
   end
+
+  def self.created_at
+    where('created_at <= ?', Time.now).order('created_at desc')
+  end
 end
