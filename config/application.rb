0f89b26a7a29eea0a293128cb6bb3e33235f8f38
Lucas Santos Rosa <luccasanttoz@gmail.com>
2018-12-02 23:48:31 -0200

    Incluindo pt-BR como idioma

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Receitasnet
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = "pt-BR"
+    I18n.config.available_locales = "pt-BR"
   end
 end
