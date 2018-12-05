8df2336c4b5200ff485f9a1fd807a1af1d38805f
Lucas Santos Rosa <luccasanttoz@gmail.com>
2018-12-05 00:37:37 -0200

    Criando formulários de comentários

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_recipe
   def index
-    @recipe=Recipe.find(params[:recipe_id])
     @comments = @recipe.comments
   end
+
+  def new
+    @comment=@recipe.comments.new
+  end
+
+  def create
+    @comment=@recipe.comments.new(comment_params)
+    if @comment.save
+      redirect_to recipe_comments_path(@recipe), notice:"Comentário gravado com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:name, :comment, :rating)
+    end
+
+    def set_recipe
+      @recipe=Recipe.find(params[:recipe_id])
+    end
 end
